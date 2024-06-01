0d379f83bc3a647b300b9c75f8a824a2e881d8a4
Abhishek yadav <yadavabhi2324@gmail.com>
2024-06-02 02:40:04 +0530

    frontend working

--- front-end/src/HomePage.jsx
+++ front-end/src/HomePage.jsx
@@ -1,23 +1,29 @@
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserList from "./userList";
+import AddUser from "./AddUser";
+// import ViewUser from "./ViewUser";
+const ViewUser = React.lazy(() => import('./ViewUser'));
+import UpdateUser from "./UpdateUser";
 
 export default function HomePage() {
-
     return (
         <>
-            <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-14">
                 <div className="bg-purple-600 drop-shadow-lg shadow-grey-500 shadow-lg text-white font-bold text-4xl p-2 rounded-lg">
                     <span>Student Mangement System</span>
                 </div>
-                <div className="flex justify-around">
-                    <div>
-                        <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Add student</button>
-                    </div>
-                    <div>
-                        <input type="text" placeholder="search..." />
-                    </div>
-                </div>
                 <div>
-                    <UserList />
+                    <BrowserRouter>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route path='/' element={<UserList />} />
+                            <Route path='/add' element={<AddUser />} />
+                            <Route path='/view' element={<ViewUser />} />
+                            <Route path='/edit' element={<UpdateUser />} />
+                        </Routes>
+                    </Suspense>
+                    </BrowserRouter>
                 </div>
             </div>
         </>
